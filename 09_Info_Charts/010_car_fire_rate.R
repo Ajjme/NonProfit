ab265c2f82b881e2d13f9bf9d08d313411beff39
Ajjme <63437142+Ajjme@users.noreply.github.com>
2023-04-05 20:39:50 -0700

    test

--- 09_Info_Charts/010_car_fire_rate.R
+++ 09_Info_Charts/010_car_fire_rate.R
@@ -1,10 +1,13 @@
 
 source("../03_Scripts/000_init.R")
-
+### inputs--------
 vehicle_type <- c("Gas", "Electric")
 fires_per_100k_sales <- c(1529.9, 25.1)
 df <- data.frame(vehicle_type, fires_per_100k_sales)
+### Source
+#Dept transportation crash data
 
+### Plot-----------------
 fig_car_fire <- plot_ly(df, x = ~vehicle_type, y = ~fires_per_100k_sales, type = 'bar', name = 'Fires per 100k Sales',
                marker = list(color = c('darkred', 'green')),
                text = paste(df$fires_per_100k_sales, 'fires per 100k sales'),
@@ -15,4 +18,7 @@ fig_car_fire <- fig %>% layout(title = "Vehicle Fires per 100k Sales",
                       yaxis = list(title = "Fires per 100k Sales"))
 
 fig_car_fire
-saveWidget(fig_car_fire, file = "vehicle_fires.html")
+
+
+### Output--------------------
+saveWidget(fig_car_fire, file = "./06_Reports_Rmd/vehicle_fires.html")
